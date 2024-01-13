32e4df89ca3bfa3cd7d492b2eebc856e2f44eca2
Alan Gonzalez <alan.gonzalez87@hotmail.com>
2024-01-13 20:30:01 -0300

    last fix

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,3 +2,11 @@ const modified = document.getElementById('lastModified');
 let currentDate = new Date(document.lastModified);
 let formattedDate = currentDate.toLocaleString();
 modified.innerHTML += formattedDate;
+
+
+// obtaining the current year
+let currentYear= new Date();
+
+let year = currentYear.getFullYear();
+
+document.getElementById("currentYear").innerText= year;
