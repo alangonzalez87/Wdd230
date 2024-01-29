720ea829c48ea3a7dbabfd0d22b69f5a562814c5
Alan Gonzalez <alan.gonzalez87@hotmail.com>
2024-01-29 14:48:54 -0300

    week3

--- scripts/script.js
+++ scripts/script.js
@@ -20,4 +20,23 @@ menuToggle.onclick=function(){
     
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener el contador de visitas desde localStorage
+    let visitCount = localStorage.getItem("visitCount");
+    
+    // Verificar si es la primera visita o si visitCount no está definido
+    if (!visitCount) {
+      visitCount = 1;
+    } else {
+      visitCount = parseInt(visitCount) + 1; // Incrementar el contador de visitas
+    }
+    
+    // Actualizar el contador de visitas en la página
+    document.getElementById("visit-count").textContent = visitCount;
+    
+    // Guardar el nuevo contador de visitas en localStorage
+    localStorage.setItem("visitCount", visitCount.toString());
+  });
+  
+
 
