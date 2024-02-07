99210a9f04df47621199c177e2ace560aea5cdb1
Alan Gonzalez <alan.gonzalez87@hotmail.com>
2024-02-06 22:50:35 -0300

    week 4 join

--- chamber/scrypt/scrypt.js
+++ chamber/scrypt/scrypt.js
@@ -40,5 +40,85 @@ document.addEventListener("DOMContentLoaded", function() {
     // Store the current visit timestamp in localStorage
     localStorage.setItem("lastVisitTimestamp", currentTimestamp);
   });
+/*membership litle scrypt for toggle */
+  document.addEventListener('DOMContentLoaded', function() {
+    const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+    
+    dropdownToggles.forEach(function(toggle) {
+        toggle.addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    });
+});
+
+/*scrypt fpr time stamp */
+document.addEventListener('DOMContentLoaded', function() {
+  
+  var timestampField = document.getElementById("timestamp");
+  
+  // Obtiene la marca de tiempo actual
+  var timestamp = new Date().toISOString();
+  
+  // Establece el valor del campo oculto con la marca de tiempo actual
+  timestampField.value = timestamp;
+});
+
+
+/*modal  */
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Evita el envío predeterminado del formulario
+  
+  // Abre una nueva ventana o modal con el mensaje de confirmación
+  var confirmationWindow = window.open("", "Confirmation", "width=400,height=200");
+  confirmationWindow.document.write("<p>¡thanks for your message!</p>");
+});
+
+
+// Cuando se envíe el formulario
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  
+  // Muestra el modal
+  modal.style.display = "block";
+
+  // Restablece el formulario
+  this.reset();
+});
+
+
+// Obtén el modal
+var modal = document.getElementById("myModal");
+
+// Verifica que el modal exista antes de intentar manipularlo
+if (modal) {
+  // Obtén el botón de cierre
+  var closeBtn = modal.querySelector(".close");
+
+  // Cuando se envíe el formulario
+  document.getElementById("myForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Evita el envío predeterminado del formulario
+      
+      // Muestra el modal
+      modal.style.display = "block";
+
+      // Restablece el formulario
+      this.reset();
+  });
+
+  // Cuando el usuario hace clic en el botón de cierre
+  if (closeBtn) {
+      closeBtn.onclick = function() {
+          modal.style.display = "none"; // Oculta el modal
+      }
+  }
+
+  // Cuando el usuario hace clic en cualquier lugar fuera del modal, ciérralo
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none"; // Oculta el modal
+      }
+  }
+}
+
   
   
